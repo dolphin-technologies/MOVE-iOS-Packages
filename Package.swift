bd761295635e1768044beca55b0410ac27d8bebc
Clemens Unterkofler <clemens.unterkofler@dolph.in>
2023-07-13 11:11:00 +0200

    fix version number

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
 			name: "DolphinMoveSDK",
-			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.4.0.250/DolphinMoveSDK.xcframework.zip",
+			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.3.4.250/DolphinMoveSDK.xcframework.zip",
 			checksum: "a7e1067235ee55be351031839503e9987639244841143ef0fad71d7d88e743de")
     ]
 )
