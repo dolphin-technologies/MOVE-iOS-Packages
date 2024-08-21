0b7d3f84779fc2f4e568b20c2fc66c370044f300
Clemens Unterkofler <clemens.unterkofler@dolph.in>
2024-08-21 15:43:29 +0200

    pusblish 2.10.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
 			name: "DolphinMoveSDK",
-			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.9.0.294/DolphinMoveSDK.xcframework.zip",
-			checksum: "78b35923d467c68c6e11255617bdbe5ffca875877acbeb3af7b8f66699a05447")
+			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.10.0.297/DolphinMoveSDK.xcframework.zip",
+			checksum: "73218f91f52d22f071fbe7dd366d339b4108a7baef9662ece38003fda9a8cc79")
     ]
 )
