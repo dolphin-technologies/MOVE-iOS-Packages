f86fc034c3a04db3c4fcb28c1eee9239538f9498
Clemens Unterkofler <clemens.unterkofler@dolph.in>
2025-12-04 09:42:16 +0100

    bugfix build

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
 			name: "DolphinMoveSDK",
-			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.15.0.326/DolphinMoveSDK.xcframework.zip",
-			checksum: "32cbfe150b1d9dc457ca5a6ef9df5a5f8a9aefe9e015eb533b5afb81940d299e"),
+			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.15.0.331/DolphinMoveSDK.xcframework.zip",
+			checksum: "df3ed81ce9478fa07db79e162f6354c4004e07ee5a755889128b75748a7f0a64"),
 		.binaryTarget(
 			name: "DolphinMoveSDKHealth",
-			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.15.0.326/DolphinMoveSDKHealth.xcframework.zip",
-			checksum: "1ca52758751c184d6927fc6de4895e77f2dab885afe9ad66bde52a3d9da47634")
+			url: "https://storage.googleapis.com/cdn.dolph.in/sdk/2.15.0.331/DolphinMoveSDKHealth.xcframework.zip",
+			checksum: "deb9e8d846a39f46670b58ed265d61759885abb970e98566f59e6004cddc28fb")
     ]
 )
